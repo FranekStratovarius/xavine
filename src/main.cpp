e7b36e8e4898324e25cc5cf2739333488181629f
FranekStratovarius <louishuss2000@gmail.com>
2022-05-30 11:11:01 +0200

    WÜÜÜRFEL!!!

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <string.h>
 #include <bx/bx.h>
+#include <bx/math.h>
 #include <bgfx/bgfx.h>
 #include <bgfx/platform.h>
 #include <GLFW/glfw3.h>
@@ -24,6 +26,75 @@
 
 #define WINDOW_WIDTH 1280
 #define WINDOW_HEIGHT 720
+
+struct PosColorVertex {
+	float x;
+	float y;
+	float z;
+	uint32_t abgr;
+};
+
+static PosColorVertex cubeVertices[] = {
+	{-1.0f,  1.0f,  1.0f, 0xff000000 },
+	{ 1.0f,  1.0f,  1.0f, 0xff0000ff },
+	{-1.0f, -1.0f,  1.0f, 0xff00ff00 },
+	{ 1.0f, -1.0f,  1.0f, 0xff00ffff },
+	{-1.0f,  1.0f, -1.0f, 0xffff0000 },
+	{ 1.0f,  1.0f, -1.0f, 0xffff00ff },
+	{-1.0f, -1.0f, -1.0f, 0xffffff00 },
+	{ 1.0f, -1.0f, -1.0f, 0xffffffff },
+};
+
+static const uint16_t cubeTriList[] = {
+	0, 1, 2,
+	1, 3, 2,
+	4, 6, 5,
+	5, 6, 7,
+	0, 2, 4,
+	4, 2, 6,
+	1, 5, 3,
+	5, 7, 3,
+	0, 4, 1,
+	4, 5, 1,
+	2, 3, 6,
+	6, 3, 7,
+};
+
+bgfx::ShaderHandle loadShader(const char *FILENAME) {
+    const char* shaderPath = "???";
+
+    switch(bgfx::getRendererType()) {
+        case bgfx::RendererType::Noop:
+        case bgfx::RendererType::Direct3D9:  shaderPath = "assets/bgfx_shaders/dx9/";   break;
+        case bgfx::RendererType::Direct3D11:
+        case bgfx::RendererType::Direct3D12: shaderPath = "assets/bgfx_shaders/dx11/";  break;
+        case bgfx::RendererType::Gnm:        shaderPath = "assets/bgfx_shaders/pssl/";  break;
+        case bgfx::RendererType::Metal:      shaderPath = "assets/bgfx_shaders/metal/"; break;
+        case bgfx::RendererType::OpenGL:     shaderPath = "assets/bgfx_shaders/glsl/";  break;
+        case bgfx::RendererType::OpenGLES:   shaderPath = "assets/bgfx_shaders/essl/";  break;
+        case bgfx::RendererType::Vulkan:     shaderPath = "assets/bgfx_shaders/spirv/"; break;
+    }
+
+    size_t shaderLen = strlen(shaderPath);
+	printf("Loading shader: %s%s\n", shaderPath, FILENAME);
+    size_t fileLen = strlen(FILENAME);
+    char *filePath = (char *)malloc(shaderLen + fileLen);
+    memcpy(filePath, shaderPath, shaderLen);
+    memcpy(&filePath[shaderLen], FILENAME, fileLen);
+
+    FILE *file = fopen(filePath, "rb");
+	printf("Loading shader: %s\n", filePath);
+    fseek(file, 0, SEEK_END);
+    long fileSize = ftell(file);
+    fseek(file, 0, SEEK_SET);
+
+    const bgfx::Memory *mem = bgfx::alloc(fileSize + 1);
+    fread(mem->data, 1, fileSize, file);
+    mem->data[mem->size - 1] = '\0';
+    fclose(file);
+
+    return bgfx::createShader(mem);
+}
 
 static bool s_showStats = false;
 
@@ -86,8 +157,24 @@ int main(void) {
 		return 1;
 	// Set view 0 to the same dimensions as the window and to clear the color buffer.
 	const bgfx::ViewId kClearView = 0;
-	bgfx::setViewClear(kClearView, BGFX_CLEAR_COLOR);
+	//bgfx::setViewClear(kClearView, BGFX_CLEAR_COLOR);
+	bgfx::setViewClear(0, BGFX_CLEAR_COLOR | BGFX_CLEAR_DEPTH, 0x443355FF, 1.0f, 0);
 	bgfx::setViewRect(kClearView, 0, 0, bgfx::BackbufferRatio::Equal);
+
+	bgfx::VertexLayout pcvDecl;
+    pcvDecl.begin()
+        .add(bgfx::Attrib::Position, 3, bgfx::AttribType::Float)
+        .add(bgfx::Attrib::Color0, 4, bgfx::AttribType::Uint8, true)
+    .end();
+    bgfx::VertexBufferHandle vbh = bgfx::createVertexBuffer(bgfx::makeRef(cubeVertices, sizeof(cubeVertices)), pcvDecl);
+    bgfx::IndexBufferHandle ibh = bgfx::createIndexBuffer(bgfx::makeRef(cubeTriList, sizeof(cubeTriList)));
+
+	bgfx::ShaderHandle vsh = loadShader("vs_cubes.bin");
+    bgfx::ShaderHandle fsh = loadShader("fs_cubes.bin");
+    bgfx::ProgramHandle program = bgfx::createProgram(vsh, fsh, true);
+
+	unsigned int counter = 0;
+
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
 		// Handle window resize.
@@ -99,6 +186,23 @@ int main(void) {
 		}
 		// This dummy draw call is here to make sure that view 0 is cleared if no other draw calls are submitted to view 0.
 		bgfx::touch(kClearView);
+
+		const bx::Vec3 at = {0.0f, 0.0f,  0.0f};
+        const bx::Vec3 eye = {0.0f, 0.0f, -5.0f};
+        float view[16];
+        bx::mtxLookAt(view, eye, at);
+        float proj[16];
+        bx::mtxProj(proj, 60.0f, float(WINDOW_WIDTH) / float(WINDOW_HEIGHT), 0.1f, 100.0f, bgfx::getCaps()->homogeneousDepth);
+        bgfx::setViewTransform(0, view, proj);
+		float mtx[16];
+    	bx::mtxRotateXY(mtx, counter * 0.01f, counter * 0.01f);
+    	bgfx::setTransform(mtx); 
+
+        bgfx::setVertexBuffer(0, vbh);
+        bgfx::setIndexBuffer(ibh);
+
+        bgfx::submit(0, program);
+
 		// Use debug font to print information about this example.
 		bgfx::dbgTextClear();
 		bgfx::dbgTextImage(bx::max<uint16_t>(uint16_t(width / 2 / 8), 20) - 20, bx::max<uint16_t>(uint16_t(height / 2 / 16), 6) - 6, 40, 12, s_logo, 160);
@@ -112,6 +216,7 @@ int main(void) {
 		bgfx::setDebug(s_showStats ? BGFX_DEBUG_STATS : BGFX_DEBUG_TEXT);
 		// Advance to next frame. Process submitted rendering primitives.
 		bgfx::frame();
+		counter++;
 	}
 	bgfx::shutdown();
 	glfwTerminate();
