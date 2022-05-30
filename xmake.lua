bc5071dcda7d3ce3c1e8c314354e5d65a246dc06
FranekStratovarius <louishuss2000@gmail.com>
2022-05-30 10:58:11 +0200

    bgfx mit glfw sollte funktionieren

--- xmake.lua
+++ xmake.lua
@@ -1,11 +1,13 @@
 set_project("xavine")
 add_rules("mode.debug", "mode.release")
-includes("graphics/xmake.lua")
-add_requires("glfw 3.3.5"--[[,"imgui v1.85","luajit 2.1.0-beta3"]],{system = false})
+--includes("graphics/xmake.lua")
+add_requires("glfw 3.3.5", "bgfx 7816","imgui v1.87-docking"--[[,"luajit 2.1.0-beta3"]],{system = false})
 
 target("xavine") do
 	set_kind("binary")
-	add_deps("xavine-graphics")
+	--add_deps("xavine-graphics")
+
+
 
 	add_files("src/main.cpp")
 	add_files("src/**.cpp")
@@ -16,6 +18,17 @@ target("xavine") do
 
 	if is_plat("linux") then
 		add_syslinks("dl")
+		add_defines("BX_PLATFORM_LINUX")
+	elseif is_plat("windows") then
+		add_defines("BX_PLATFORM_WINDOWS")
+	elseif is_plat("macosx") then
+		add_defines("BX_PLATFORM_OSX")
 	end
-	add_packages("glfw"--[[,"imgui","luajit"]],"xavine_graphics")
+	--add_packages("glfw"--[[,"imgui","luajit"]],"xavine_graphics")
+	add_packages("glfw", "imgui", "bgfx")
+
+	-- copy asset folder after build
+	after_build(function (target)
+		os.cp(path.join("assets"), path.join("$(buildir)", "$(os)", "$(arch)", "$(mode)"))
+	end)
 end
