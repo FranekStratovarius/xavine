cd081631a0ee9dd668d88d0ccc5fca4363a40211
FranekStratovarius <louishuss2000@gmail.com>
2022-06-03 19:36:50 +0200

    bgfx-mine added

--- xmake.lua
+++ xmake.lua
@@ -1,6 +1,7 @@
 set_project("xavine")
 add_rules("mode.debug", "mode.release")
-add_requires("glfw 3.3.5", "bgfx 7816","imgui v1.87-docking",{system = false})
+add_repositories("xavine-repo https://github.com/FranekStratovarius/xavine-repo main")
+add_requires("glfw 3.3.5", "bgfx-mine 7816","imgui v1.87-docking",{system = false})
 
 target("xavine") do
 	set_kind("binary")
@@ -11,12 +12,6 @@ target("xavine") do
 	if is_plat("windows") then
 		add_includedirs("C:\\Users\\louis\\AppData\\Local\\.xmake\\packages\\b\\bgfx\\7816\\e024fd36069a4b5b83561fec3bb8fd07\\include\\compat\\msvc")
 	end
-
-	on_load(function (target)
-        import("lib.detect.find_library")
-        local package = find_library("bgfx")
-		print("bgfx path:", package.includedir)
-    end)
 
 	set_warnings("all")
 	set_optimize("fastest")
@@ -31,10 +26,14 @@ target("xavine") do
 		add_defines("BX_PLATFORM_OSX")
 	end
 
-	add_packages("glfw", "imgui", "bgfx")
+	add_packages("glfw", "imgui", "bgfx-mine")
 
 	-- copy asset folder after build
 	after_build(function (target)
 		os.cp(path.join("assets"), path.join("$(buildir)", "$(os)", "$(arch)", "$(mode)"))
+	end)
+
+	on_package(function (target)
+		os.cp(path.join("assets"), path.join("$(buildir)", "packages", "x", "xavine", "$(os)", "$(arch)", "$(mode)", "bin"))
 	end)
 end
