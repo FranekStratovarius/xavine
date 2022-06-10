42077f147216614e4a78979a575a1ede12f4e330
FranekStratovarius <louishuss2000@gmail.com>
2022-06-10 12:20:53 +0200

    extracted glfw and bgfx graphics into module - todo: shared libs

--- src/main.cpp
+++ src/main.cpp
@@ -1,231 +1,32 @@
 #include <stdio.h>
-#include <string.h>
-#include <bx/bx.h>
-#include <bx/math.h>
-#include <bgfx/bgfx.h>
-#include <bgfx/platform.h>
-#include <GLFW/glfw3.h>
-
-#if BX_PLATFORM_LINUX || BX_PLATFORM_BSD
-#	if ENTRY_CONFIG_USE_WAYLAND
-#		include <wayland-egl.h>
-#		define GLFW_EXPOSE_NATIVE_WAYLAND
-#	else
-#		define GLFW_EXPOSE_NATIVE_X11
-#		define GLFW_EXPOSE_NATIVE_GLX
-#	endif
-#elif BX_PLATFORM_OSX
-#	define GLFW_EXPOSE_NATIVE_COCOA
-#	define GLFW_EXPOSE_NATIVE_NSGL
-#elif BX_PLATFORM_WINDOWS
-#	define GLFW_EXPOSE_NATIVE_WIN32
-#	define GLFW_EXPOSE_NATIVE_WGL
-#endif //
-#include <GLFW/glfw3native.h>
-
-#define WINDOW_WIDTH 1280
-#define WINDOW_HEIGHT 720
-
-struct PosColorVertex
-{
-	float x;
-	float y;
-	float z;
-	uint32_t abgr;
-};
-
-static PosColorVertex cubeVertices[] =
-{
-	{-1.0f,  1.0f,  1.0f, 0xff000000 },
-	{ 1.0f,  1.0f,  1.0f, 0xff0000ff },
-	{-1.0f, -1.0f,  1.0f, 0xff00ff00 },
-	{ 1.0f, -1.0f,  1.0f, 0xff00ffff },
-	{-1.0f,  1.0f, -1.0f, 0xffff0000 },
-	{ 1.0f,  1.0f, -1.0f, 0xffff00ff },
-	{-1.0f, -1.0f, -1.0f, 0xffffff00 },
-	{ 1.0f, -1.0f, -1.0f, 0xffffffff },
-};
-
-static const uint16_t cubeTriList[] =
-{
-	0, 1, 2,
-	1, 3, 2,
-	4, 6, 5,
-	5, 6, 7,
-	0, 2, 4,
-	4, 2, 6,
-	1, 5, 3,
-	5, 7, 3,
-	0, 4, 1,
-	4, 5, 1,
-	2, 3, 6,
-	6, 3, 7,
-};
-
-bgfx::ShaderHandle loadShader(const char *FILENAME)
-{
-    const char* shaderPath = "???";
-
-    switch(bgfx::getRendererType())
-	{
-        case bgfx::RendererType::Noop:
-        case bgfx::RendererType::Direct3D9:  shaderPath = "assets/shaders/dx9/";   break;
-        case bgfx::RendererType::Direct3D11: shaderPath = "assets/shaders/dx11/";  break;
-        case bgfx::RendererType::Direct3D12: shaderPath = "assets/shaders/dx12/";  break;
-        case bgfx::RendererType::Gnm:        shaderPath = "assets/shaders/pssl/";  break;
-        case bgfx::RendererType::Metal:      shaderPath = "assets/shaders/metal/"; break;
-        case bgfx::RendererType::OpenGL:     shaderPath = "assets/shaders/glsl/";  break;
-        case bgfx::RendererType::OpenGLES:   shaderPath = "assets/shaders/essl/";  break;
-        case bgfx::RendererType::Vulkan:     shaderPath = "assets/shaders/spirv/"; break;
-    }
-
-    size_t shaderLen = strlen(shaderPath);
-	printf("Loading shader: %s%s\n", shaderPath, FILENAME);
-    size_t fileLen = strlen(FILENAME);
-    char *filePath = (char *)malloc(shaderLen + fileLen);
-    memcpy(filePath, shaderPath, shaderLen);
-    memcpy(&filePath[shaderLen], FILENAME, fileLen);
-
-    FILE *file = fopen(filePath, "rb");
-	printf("Loading shader: %s\n", filePath);
-    fseek(file, 0, SEEK_END);
-    long fileSize = ftell(file);
-    fseek(file, 0, SEEK_SET);
-
-    const bgfx::Memory *mem = bgfx::alloc(fileSize + 1);
-    fread(mem->data, 1, fileSize, file);
-    mem->data[mem->size - 1] = '\0';
-    fclose(file);
-
-    return bgfx::createShader(mem);
-}
-
-static bool s_showStats = false;
-
-static void glfw_errorCallback(int error, const char *description)
-{
-	fprintf(stderr, "GLFW error %d: %s\n", error, description);
-}
-
-static void glfw_keyCallback(GLFWwindow *window, int key, int scancode, int action, int mods)
-{
-	if (key == GLFW_KEY_F1 && action == GLFW_RELEASE)
-		s_showStats = !s_showStats;
-}
+#include "graphics_init.h"
 
 int main(void)
 {
-	// Create a GLFW window without an OpenGL context.
-	glfwSetErrorCallback(glfw_errorCallback);
-	if (!glfwInit())
-		return 1;
-	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-	GLFWwindow *window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "bgfx", nullptr, nullptr);
-	if (!window)
-		return 1;
-	glfwSetKeyCallback(window, glfw_keyCallback);
-	// Call bgfx::renderFrame before bgfx::init to signal to bgfx not to create a render thread.
-	// Most graphics APIs must be used on the same thread that created the window.
-	bgfx::renderFrame();
-	// Initialize bgfx using the native window handle and window resolution.
-	bgfx::Init init;
-#if BX_PLATFORM_LINUX || BX_PLATFORM_BSD
-# 		if ENTRY_CONFIG_USE_WAYLAND
-		wl_egl_window *win_impl = (wl_egl_window*)glfwGetWindowUserPointer(window);
-		if(!win_impl)
-		{
-			int width, height;
-			glfwGetWindowSize(window, &width, &height);
-			struct wl_surface* surface = (struct wl_surface*)glfwGetWaylandWindow(window);
-			if(!surface)
-				return nullptr;
-			win_impl = wl_egl_window_create(surface, width, height);
-			glfwSetWindowUserPointer(window, (void*)(uintptr_t)win_impl);
-		}
-		init.platformData.nwh = (void*)(uintptr_t)win_impl;
-		init.platformData.ndt = glfwGetWaylandDisplay();
-#		else
-		init.platformData.nwh = (void*)(uintptr_t)glfwGetX11Window(window);
-		init.platformData.ndt = glfwGetX11Display();
-#		endif
-#elif BX_PLATFORM_OSX
-	init.platformData.nwh = glfwGetCocoaWindow(window);
-#elif BX_PLATFORM_WINDOWS
-	init.platformData.nwh = glfwGetWin32Window(window);
-#endif
-	int width, height;
-	glfwGetWindowSize(window, &width, &height);
-	init.resolution.width = (uint32_t)width;
-	init.resolution.height = (uint32_t)height;
-	init.resolution.reset = BGFX_RESET_VSYNC;
-	if (!bgfx::init(init))
-		return 1;
-	// Set view 0 to the same dimensions as the window and to clear the color buffer.
-	const bgfx::ViewId kClearView = 0;
-	//bgfx::setViewClear(kClearView, BGFX_CLEAR_COLOR);
-	bgfx::setViewClear(0, BGFX_CLEAR_COLOR | BGFX_CLEAR_DEPTH, 0x443355FF, 1.0f, 0);
-	bgfx::setViewRect(kClearView, 0, 0, bgfx::BackbufferRatio::Equal);
-
-	bgfx::VertexLayout pcvDecl;
-    pcvDecl.begin()
-        .add(bgfx::Attrib::Position, 3, bgfx::AttribType::Float)
-        .add(bgfx::Attrib::Color0, 4, bgfx::AttribType::Uint8, true)
-    .end();
-    bgfx::VertexBufferHandle vbh = bgfx::createVertexBuffer(bgfx::makeRef(cubeVertices, sizeof(cubeVertices)), pcvDecl);
-    bgfx::IndexBufferHandle ibh = bgfx::createIndexBuffer(bgfx::makeRef(cubeTriList, sizeof(cubeTriList)));
-
-	bgfx::ShaderHandle vsh = loadShader("vs_base.bin");
-    bgfx::ShaderHandle fsh = loadShader("fs_base.bin");
-    bgfx::ProgramHandle program = bgfx::createProgram(vsh, fsh, true);
-
-	unsigned int counter = 0;
-
-	while (!glfwWindowShouldClose(window))
+	// initialization
+	xavine::Renderer* renderer;
+	try
 	{
-		glfwPollEvents();
-		// Handle window resize.
-		int oldWidth = width, oldHeight = height;
-		glfwGetWindowSize(window, &width, &height);
-		if (width != oldWidth || height != oldHeight)
-		{
-			bgfx::reset((uint32_t)width, (uint32_t)height, BGFX_RESET_VSYNC);
-			bgfx::setViewRect(kClearView, 0, 0, bgfx::BackbufferRatio::Equal);
-		}
-		// This dummy draw call is here to make sure that view 0 is cleared if no other draw calls are submitted to view 0.
-		bgfx::touch(kClearView);
-
-		const bx::Vec3 at = {0.0f, 0.0f,  0.0f};
-        const bx::Vec3 eye = {0.0f, 0.0f, -5.0f};
-        float view[16];
-        bx::mtxLookAt(view, eye, at);
-        float proj[16];
-        bx::mtxProj(proj, 60.0f, float(WINDOW_WIDTH) / float(WINDOW_HEIGHT), 0.1f, 100.0f, bgfx::getCaps()->homogeneousDepth);
-        bgfx::setViewTransform(0, view, proj);
-		float mtx[16];
-    	bx::mtxRotateXY(mtx, counter * 0.01f, counter * 0.01f);
-    	bgfx::setTransform(mtx); 
-
-        bgfx::setVertexBuffer(0, vbh);
-        bgfx::setIndexBuffer(ibh);
-
-        bgfx::submit(0, program);
-
-		// Use debug font to print information about this example.
-		bgfx::dbgTextClear();
-		bgfx::dbgTextPrintf(0, 0, 0x0f, "Press F1 to toggle stats.");
-		bgfx::dbgTextPrintf(0, 1, 0x0f, "Color can be changed with ANSI \x1b[9;me\x1b[10;ms\x1b[11;mc\x1b[12;ma\x1b[13;mp\x1b[14;me\x1b[0m code too.");
-		bgfx::dbgTextPrintf(80, 1, 0x0f, "\x1b[;0m    \x1b[;1m    \x1b[; 2m    \x1b[; 3m    \x1b[; 4m    \x1b[; 5m    \x1b[; 6m    \x1b[; 7m    \x1b[0m");
-		bgfx::dbgTextPrintf(80, 2, 0x0f, "\x1b[;8m    \x1b[;9m    \x1b[;10m    \x1b[;11m    \x1b[;12m    \x1b[;13m    \x1b[;14m    \x1b[;15m    \x1b[0m");
-		const bgfx::Stats* stats = bgfx::getStats();
-		bgfx::dbgTextPrintf(0, 2, 0x0f, "Backbuffer %dW x %dH in pixels, debug text %dW x %dH in characters.", stats->width, stats->height, stats->textWidth, stats->textHeight);
-		// Enable stats or debug text.
-		bgfx::setDebug(s_showStats ? BGFX_DEBUG_STATS : BGFX_DEBUG_TEXT);
-		// Advance to next frame. Process submitted rendering primitives.
-		bgfx::frame();
-		counter++;
+		renderer = new xavine::Renderer();
 	}
-	bgfx::shutdown();
-	glfwTerminate();
+	catch(const char* message)
+	{
+		fprintf(stderr, "initialization failed: %s\n", message);
+		return 1;
+	}
+
+	if (renderer == nullptr)
+	{
+		fprintf(stderr, "no renderer created\n");
+		return 1;
+	}
+
+
+	// render loop
+	while (renderer->render());
+
+	// cleanup
+	delete renderer;
 
 	return 0;
 }
--- xmake.lua
+++ xmake.lua
@@ -1,7 +1,11 @@
 set_project("xavine")
 add_rules("mode.debug", "mode.release")
 add_repositories("xavine-xrepo https://github.com/FranekStratovarius/xmake-repo master")
-add_requires("glfw 3.3.5", "bgfx 7816", "imgui v1.87-docking", "flecs v3.0.1-alpha", {system = false})
+add_requires("bgfx 7816", "flecs v3.0.1-alpha", "glfw 3.3.5", {system = false})
+add_requireconfs("bgfx")
+add_requireconfs("flecs", {configs = {shared = true}})
+add_requireconfs("glfw", {configs = {shared = true}})
+includes("modules/graphics/xmake.lua")
 
 target("xavine") do
 	set_kind("binary")
@@ -13,71 +17,13 @@ target("xavine") do
 	set_warnings("all")
 	set_optimize("fastest")
 
-	-- set bgfx platform defines
-	if is_plat("linux") then
-		add_syslinks("dl")
-		add_defines("BX_PLATFORM_LINUX")
-	elseif is_plat("windows") then
-		add_defines("BX_PLATFORM_WINDOWS")
-	elseif is_plat("macosx") then
-		add_defines("BX_PLATFORM_OSX")
-	end
-
-	add_packages("glfw", "imgui", "bgfx", "flecs")
+	add_packages("bgfx", "flecs", "glfw")
+	add_deps("xavine-graphics")
 
 	-- copy asset folder after build
 	after_build(function (target)
 		-- copy assets to build output folder
 		os.cp(path.join("assets"), path.join("$(buildir)", "$(os)", "$(arch)", "$(mode)"))
-
-		-- list of shader models
-		local shader_models = {
-			-- OpenGL ES Shading Language / WebGL (ESSL)
-			{lang = "essl", id = "320_es"},
-
-			-- Metal Shading Language (MSL)
-			{lang = "metal", id = "metal"},
-			
-			-- Standard Portable Intermediate Representation - V (SPIR-V) [Vulkan]
-			{lang = "spirv", id = "spirv15-12"},
-
-			-- OpenGL Shading Language (GLSL)
-			--{lang = "glsl", id = 150},
-			{lang = "glsl", id = "440"},
-		}
-
-		-- High-Level Shading Language (HLSL) [DirectX] only on windows
-		if os.host == "windows" then
-			table.append({lang = "dx9",  id = "s_3_0"})
-			table.append({lang = "dx11", id = "s_4_0"})
-			table.append({lang = "dx12", id = "s_5_0"})
-		end
-
-		-- compile shaders and output them in the build output folder
-		for _, shader_model in ipairs(shader_models) do
-			-- create output folder
-			os.mkdir(vformat(path.join("$(buildir)", "$(os)", "$(arch)", "$(mode)", "assets", "shaders", shader_model.lang)))
-			-- compile fragment and vertex shader
-			for _, shader_type in ipairs({
-				{long = "fragment", short = "fs"},
-				{long = "vertex", short = "vs"},
-			}) do
-				-- compile multiple shaders
-				for _, file_path in ipairs(os.files(path.join("$(projectdir)", "shaders", shader_type.short.."_*.sc"))) do
-					os.exec(
-						path.join("tools", "bgfx", "$(os)", "$(arch)", "shadercRelease")
-						..vformat(" -f "..file_path)
-						..vformat(" -o "..path.join("$(buildir)", "$(os)", "$(arch)", "$(mode)", "assets", "shaders", shader_model.lang, path.basename(file_path)..".bin"))
-						..vformat(" --platform $(os)")
-						..vformat(" -p "..shader_model.id)
-						..vformat(" --type "..shader_type.long)
-						..vformat(" --varyingdef "..path.join("$(projectdir)", "shaders", "varying.def.sc"))
-						--..vformat(" --verbose")
-						..vformat(" -i "..path.join("$(projectdir)", "shaders", "src"))
-					)
-				end	-- file_path
-			end	-- shader_type
-		end	-- shader_model
 	end)
 
 	--[[
