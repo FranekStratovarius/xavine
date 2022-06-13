85deab0cff8cd222419ca8e87c53fb26fc22b64a
FranekStratovarius <louishuss2000@gmail.com>
2022-06-13 10:58:03 +0200

    custom rule for shader compiling finished

--- xmake.lua
+++ xmake.lua
@@ -5,6 +5,67 @@ add_requireconfs("bgfx")
 add_requireconfs("flecs", {configs = {shared = true}})
 add_requireconfs("glfw", {configs = {shared = true}})
 
+rule("shader") do
+	set_extensions(".sc")
+	on_buildcmd_file(function (target, batchcmds, sourcefile, opt)
+		-- list of shader models
+		local shader_models = {
+			-- OpenGL ES Shading Language / WebGL (ESSL)
+			{lang = "essl", id = "320_es"},
+			-- Metal Shading Language (MSL)
+			{lang = "metal", id = "metal"},
+			-- Standard Portable Intermediate Representation - V (SPIR-V) [Vulkan]
+			{lang = "spirv", id = "spirv15-12"},
+			-- OpenGL Shading Language (GLSL)
+			{lang = "glsl", id = "440"},
+		}
+
+		-- High-Level Shading Language (HLSL) [DirectX] only on windows
+		if is_plat("windows") then
+			table.insert(shader_models, {lang = "dx9",  id = "s_3_0", dx = true})
+			table.insert(shader_models, {lang = "dx11", id = "s_4_0", dx = true})
+			table.insert(shader_models, {lang = "dx12", id = "s_5_0", dx = true})
+		end
+
+		-- compile shaders and output them in the build output folder
+		for _, shader_model in ipairs(shader_models) do
+			-- make sure build directory exists
+			batchcmds:mkdir(path.join(target:targetdir(), "assets", "shaders", shader_model.lang))
+
+			-- get type of shader
+			local shader_type = string.sub(path.basename(sourcefile), 1, 2)
+			local shader_tags = nil
+			if shader_type == "fs" then
+				shader_tags = {long = "fragment", short = "p"}
+			elseif shader_type == "vs" then
+				shader_tags = {long = "vertex  ", short = "p"}
+			elseif shader_type == "cs" then
+				shader_tags = {long = "compute ", short = "p"}
+			else
+				return
+			end
+
+			-- compile shader
+			local targetfile = path.join(target:targetdir(), "assets", "shaders", shader_model.lang, path.basename(sourcefile)..".bin")
+			batchcmds:vrunv(
+				path.join(target:pkgs()["bgfx"]:installdir(), "bin", "shadercRelease")
+				.." -f "..sourcefile
+				.." -o "..targetfile
+				..vformat(" --platform $(os)")
+				.." --profile "..(shader_model.dx and shader_tags.short or "")..shader_model.id
+				.." --type "..shader_tags.long
+				..vformat(" --varyingdef "..path.join("$(projectdir)", "shaders", "varying.def.sc"))
+				--..vformat(" --verbose")
+				..vformat(" -i "..path.join("$(projectdir)", "shaders", "src"))
+			)
+			batchcmds:show_progress(opt.progress, "${color.build.object}compiling "..shader_tags.long.." shader %s to %s", sourcefile, targetfile)
+		end	-- shader_model
+
+		-- only rebuild the file if its changed since last run
+		batchcmds:add_depfiles(sourcefile)
+    end)
+end
+
 target("xavine") do
 	set_kind("binary")
 	set_languages("cxx11")
@@ -25,63 +86,15 @@ target("xavine") do
 		add_defines("BX_PLATFORM_OSX")
 	end
 
+	add_rules("shader")
+	add_files("shaders/**.sc")
+
 	add_packages("bgfx", "flecs", "glfw")
 
 	-- copy asset folder after build
 	after_build(function (target)
 		-- copy assets to build output folder
 		os.cp(path.join("assets"), path.join("$(buildir)", "$(os)", "$(arch)", "$(mode)"))
-		
-		-- list of shader models
-		-- shadercompilierung auf custom rule umbauen: https://xmake.io/#/manual/custom_rule
-		local shader_models = {
-			-- OpenGL ES Shading Language / WebGL (ESSL)
-			{lang = "essl", id = "320_es"},
-
-			-- Metal Shading Language (MSL)
-			{lang = "metal", id = "metal"},
-			
-			-- Standard Portable Intermediate Representation - V (SPIR-V) [Vulkan]
-			{lang = "spirv", id = "spirv15-12"},
-
-			-- OpenGL Shading Language (GLSL)
-			--{lang = "glsl", id = 150},
-			{lang = "glsl", id = "440"},
-		}
-
-		-- High-Level Shading Language (HLSL) [DirectX] only on windows
-		if is_plat("windows") then
-			table.insert(shader_models, {lang = "dx9",  id = "s_3_0", dx = true})
-			table.insert(shader_models, {lang = "dx11", id = "s_4_0", dx = true})
-			table.insert(shader_models, {lang = "dx12", id = "s_5_0", dx = true})
-		end
-
-		-- compile shaders and output them in the build output folder
-		for _, shader_model in ipairs(shader_models) do
-			-- create output folder
-			os.mkdir(vformat(path.join("$(buildir)", "$(os)", "$(arch)", "$(mode)", "assets", "shaders", shader_model.lang)))
-			-- compile fragment and vertex shader
-			for _, shader_type in ipairs({
-				{long = "fragment", short = "fs", shorter = "p"},
-				{long = "vertex", short = "vs", shorter = "v"},
-				{long = "compute", short = "cs", shorter = "c"},
-			}) do
-				-- compile multiple shaders
-				for _, file_path in ipairs(os.files(path.join("$(projectdir)", "shaders", shader_type.short.."_*.sc"))) do
-					os.exec(
-						path.join(target:pkgs()["bgfx"]:installdir(), "bin", "shadercRelease")
-						..vformat(" -f "..file_path)
-						..vformat(" -o "..path.join("$(buildir)", "$(os)", "$(arch)", "$(mode)", "assets", "shaders", shader_model.lang, path.basename(file_path)..".bin"))
-						..vformat(" --platform $(os)")
-						..vformat(" --profile "..(shader_model.dx and shader_model.shorter or "")..shader_model.id)
-						..vformat(" --type "..shader_type.long)
-						..vformat(" --varyingdef "..path.join("$(projectdir)", "shaders", "varying.def.sc"))
-						--..vformat(" --verbose")
-						..vformat(" -i "..path.join("$(projectdir)", "shaders", "src"))
-					)
-				end	-- file_path
-			end	-- shader_type
-		end	-- shader_model
 	end)
 
 	--[[
